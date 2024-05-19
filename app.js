70b33e0edf0ae9b6ee6bf46949df38d1ef4fd707
vishalsoni2001 <112778682+vishalsoni2001@users.noreply.github.com>
2024-05-19 12:45:39 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,25 @@
+// const heading =React.createElement("h1",{id:"heading" },"Hello from React");
+
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+
+// <div id="parent">
+//     <div id="child">
+//     <h1>
+//      I am h1 tag
+//     </h1>
+//     </div>
+// </div>
+
+const parent=React.createElement("div",{id:"parent"},
+ React.createElement("div",{id:"child"},
+ React.createElement("h1",{},"I am h1 tag")
+ )
+
+);
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
